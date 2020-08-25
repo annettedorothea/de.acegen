e623ad2e9b9998057f5816df97756eef488c5618
Annette Pohl <annette_pohl@web.de>
2020-08-25 05:25:10 +0200

    start version 0.9.10

--- de.acegen/xtend-gen/de/acegen/extensions/CommonExtension.java
+++ de.acegen/xtend-gen/de/acegen/extensions/CommonExtension.java
@@ -64,7 +64,7 @@ public class CommonExtension {
     _builder.append("*");
     _builder.newLine();
     _builder.append(" ");
-    _builder.append("* generated with de.acegen 0.9.9");
+    _builder.append("* generated with de.acegen 0.9.10");
     _builder.newLine();
     _builder.append(" ");
     _builder.append("*");
